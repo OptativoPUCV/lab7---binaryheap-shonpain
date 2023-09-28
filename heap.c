9666de6125ea064c837c39394122359414c9431a
Replit user <>
2023-09-28 06:49:32 +0000

    new try

--- heap.c
+++ heap.c
@@ -19,13 +19,18 @@ typedef struct Heap{
 
 void* heap_top(Heap* pq){
     if (pq->size == 0)return NULL;
-
     return pq->heapArray[0].data;
 }
 
 
-
 void heap_push(Heap* pq, void* data, int priority){
+    if (pq->size >= pq->capac) {
+        // Aumentar la capacidad al doble + 1
+        int nueva_capacidad = pq->capac * 2 + 1;
+        pq->heapArray = realloc(pq->heapArray, nueva_capacidad * sizeof(heapElem));
+        if (pq->heapArray == NULL)exit(EXIT_FAILURE);
+        pq->capac = nueva_capacidad;
+    }
 
 }
 
